0ec9c6712217837a83030413ff5edcd84651b2f2
Imanol Espinal <127232789+imaesp@users.noreply.github.com>
2025-03-20 22:45:07 -0400

    Add files via upload

---
+++ pacman.py
@@ -0,0 +1,43 @@
+# Build Pac-Man from Scratch in Python with PyGame!!
+import pygame
+
+
+pygame.init()
+
+WIDTH = 900
+HEIGHT = 950
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 20)
+
+
+
+run = True
+while run:
+    timer.tick(fps)
+    if counter < 19:
+        counter += 1
+        if counter > 3:
+            flicker = False
+    else:
+        counter = 0
+        flicker = True
+    if powerup and power_counter < 600:
+        power_counter += 1
+    elif powerup and power_counter >= 600:
+        power_counter = 0
+        powerup = False
+        eaten_ghost = [False, False, False, False]
+    if startup_counter < 180 and not game_over and not game_won:
+        moving = False
+        startup_counter += 1
+    else:
+        moving = True
+
+    screen.fill('black')
+    draw_board()
+
+  
+pygame.quit()
+
